113f571bdd47dda79bc41120bce060bd2b010eb2
Junaid Vallimohamed <junaid-vallimohamed@outlook.com>
2022-10-29 23:27:58 +0100

    .

--- index.php
+++ index.php
@@ -11,17 +11,34 @@ mysqli_ssl_set($con,NULL,NULL, "DigiCertGlobalRootCA.crt.pem", NULL, NULL);
 mysqli_real_connect($con, "note95-server.mysql.database.azure.com", "sidewerzsl", "G4M516SRX7M6BUEM$", "note95-database", 3306, MYSQLI_CLIENT_SSL);
 
 // sql to create table
-$sql = "CREATE TABLE `note_table` (
-  `id` int(255) NOT NULL,
-  `note` mediumtext NOT NULL,
-  `tags` mediumtext NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;";
+$sql = "INSERT INTO `note_table` (`id`, `note`, `tags`) VALUES
+(15, 'Hello World!', ''),
+(16, 'This is note #2', 'health')";
 
 if (mysqli_query($con, $sql)) {
-  echo "Table MyGuests created successfully";
+  echo "Query 1";
 } else {
-  echo "Error creating table: " . mysqli_error($con);
+  echo "Error: " . mysqli_error($con);
 }
+
+$sql = "ALTER TABLE `note_table`
+  ADD PRIMARY KEY (`id`)";
+
+if (mysqli_query($con, $sql)) {
+  echo "Query 2";
+} else {
+  echo "Error: " . mysqli_error($con);
+}
+
+$sql = "ALTER TABLE `note_table`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17";
+
+if (mysqli_query($con, $sql)) {
+  echo "Query 3";
+} else {
+  echo "Error: " . mysqli_error($con);
+}
+
 
 //Close the connection
 mysqli_close($con);
