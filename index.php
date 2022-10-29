092a36630babc19d183be5063e789f7e3e5599fc
Junaid Vallimohamed <junaid-vallimohamed@outlook.com>
2022-10-29 21:11:28 +0100

    ..

--- index.php
+++ index.php
@@ -1,6 +1,18 @@
 <?php
 
-echo "Hi??";
+$host = 'note95-server.mysql.database.azure.com';
+$username = 'sidewerzsl';
+$password = 'G4M516SRX7M6BUEM$';
+$db_name = 'note95-database';
 
+//Establishes the connection
+$conn = mysqli_init();
+mysqli_real_connect($conn, $host, $username, $password, $db_name, 3306);
+if (mysqli_connect_errno($conn)) {
+die('Failed to connect to MySQL: '.mysqli_connect_error());
+}
+
+//Close the connection
+mysqli_close($conn);
 ?>
 Hello....
