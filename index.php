33e39d0e7565367c5919208a0e9534687f4504e9
Junaid Vallimohamed <junaid-vallimohamed@outlook.com>
2022-10-29 22:57:45 +0100

    .

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ $db_name = 'note95-db';
 //Establishes the connection
 $con = mysqli_init(); 
 mysqli_ssl_set($con,NULL,NULL, "DigiCertGlobalRootCA.crt.pem", NULL, NULL); 
-mysqli_real_connect($conn, "note95-server.mysql.database.azure.com", "sidewerzsl", "G4M516SRX7M6BUEM$", "note-database", 3306, MYSQLI_CLIENT_SSL);
+mysqli_real_connect($conn, "note95-server.mysql.database.azure.com", "sidewerzsl", "G4M516SRX7M6BUEM$", "note95-database", 3306, MYSQLI_CLIENT_SSL);
 
 //Close the connection
 mysqli_close($conn);
