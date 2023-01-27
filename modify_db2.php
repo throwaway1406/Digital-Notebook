682ac7187ab057ff75f84a1c26e9e5aef4da0e78
throwaway1406 <115081741+throwaway1406@users.noreply.github.com>
2023-01-27 13:19:17 +0000

    Create modify_db2.php

---
+++ modify_db2.php
@@ -0,0 +1,25 @@
+<?php
+
+$conn = mysqli_init(); 
+	mysqli_ssl_set($conn,NULL,NULL, "DigiCertGlobalRootCA.crt.pem", NULL, NULL); 
+	mysqli_real_connect($conn, "digitalnotes-server.mysql.database.azure.com", "abvnnoaoog", "005D4TF81D36FI4I$", "digitalnotes-database", 3306, MYSQLI_CLIENT_SSL);
+
+$sql = "CREATE TABLE `notes_table` (
+  `id` int(255) NOT NULL AUTO_INCREMENT,
+  `title` mediumtext NOT NULL,
+  `note` mediumtext NOT NULL,
+  `tags` mediumtext NOT NULL,
+  `date` mediumtext NOT NULL,
+  PRIMARY KEY (id)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1";
+
+ $result = mysqli_query($conn, $sql);
+
+    if (mysqli_query($conn, $sql)) {
+      echo "Query 1";
+    } else {
+      echo "Error: " . mysqli_error($conn);
+    }
+
+    mysqli_close($conn);
+?>
