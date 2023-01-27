d0a085224ab2c23010b6e0b1e662c33c8dddc724
throwaway1406 <115081741+throwaway1406@users.noreply.github.com>
2023-01-27 12:49:28 +0000

    Update modify_db.php

--- modify_db.php
+++ modify_db.php
@@ -2,7 +2,7 @@
 
     $conn = mysqli_init(); 
 	mysqli_ssl_set($conn,NULL,NULL, "DigiCertGlobalRootCA.crt.pem", NULL, NULL); 
-	mysqli_real_connect($conn, "note95-server.mysql.database.azure.com", "sidewerzsl", "G4M516SRX7M6BUEM$", "note95-database", 3306, MYSQLI_CLIENT_SSL);
+	mysqli_real_connect($conn, "digitalnotes-server.mysql.database.azure.com", "abvnnoaoog", "005D4TF81D36FI4I$", "digitalnotes-database", 3306, MYSQLI_CLIENT_SSL);
 
 
 $sql = "CREATE TABLE `note_table` (
