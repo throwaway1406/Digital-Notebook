c22f9809796672c144ab91e43dbee8bf277c55eb
throwaway1406 <115081741+throwaway1406@users.noreply.github.com>
2023-01-27 12:46:55 +0000

    Update modify_db.php

--- modify_db.php
+++ modify_db.php
@@ -5,7 +5,30 @@
 	mysqli_real_connect($conn, "note95-server.mysql.database.azure.com", "sidewerzsl", "G4M516SRX7M6BUEM$", "note95-database", 3306, MYSQLI_CLIENT_SSL);
 
 
-    $sql = "ALTER TABLE `note_table` ADD `title` mediumtext NOT NULL AFTER `id`";
+$sql = "CREATE TABLE `note_table` (
+  `id` int(255) NOT NULL,
+  `title` mediumtext NOT NULL,
+  `note` mediumtext NOT NULL,
+  `tags` mediumtext NOT NULL,
+  `date` mediumtext NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1";
+
+
+$sql2 = "INSERT INTO `note_table` (`id`, `title`, `note`, `tags`, `date`) VALUES
+(17, '', 'This is note #3. It has multiple tags.', 'tag1, tag2, tag3', '2022-10-30'),
+(19, '', 'This is a note created since a &#039;date&#039; column has been added to the DB!', '', '2022-10-30'),
+(26, 'lorem', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', 'test', '30/10/2022 14:30:33')";
+
+
+
+$sql3 = "ALTER TABLE `note_table`
+  ADD PRIMARY KEY (`id`)";
+
+
+$sql4 = "ALTER TABLE `note_table`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
+COMMIT";
+
     $result = mysqli_query($conn, $sql);
 
     if (mysqli_query($conn, $sql)) {
@@ -14,11 +37,26 @@
       echo "Error: " . mysqli_error($conn);
     }
 
-    $sql = "ALTER TABLE `note_table` ADD `date` mediumtext NOT NULL AFTER `tags`";
-    $result = mysqli_query($conn, $sql);
+    $result = mysqli_query($conn, $sql2);
 
-    if (mysqli_query($conn, $sql)) {
+    if (mysqli_query($conn, $sql2)) {
       echo "Query 2";
+    } else {
+      echo "Error: " . mysqli_error($conn);
+    }
+
+    $result = mysqli_query($conn, $sql3);
+
+    if (mysqli_query($conn, $sql3)) {
+      echo "Query 3";
+    } else {
+      echo "Error: " . mysqli_error($conn);
+    }
+
+    $result = mysqli_query($conn, $sql4);
+
+    if (mysqli_query($conn, $sql4)) {
+      echo "Query 4";
     } else {
       echo "Error: " . mysqli_error($conn);
     }
