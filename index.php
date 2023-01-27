9a107d87957a14d977d40e2e8bcd94419691ad64
throwaway1406 <115081741+throwaway1406@users.noreply.github.com>
2023-01-27 12:49:13 +0000

    Update index.php

--- index.php
+++ index.php
@@ -364,7 +364,7 @@ if (isset($_GET['id'])) {
 
     $conn = mysqli_init(); 
 	mysqli_ssl_set($conn,NULL,NULL, "DigiCertGlobalRootCA.crt.pem", NULL, NULL); 
-	mysqli_real_connect($conn, "note95-server.mysql.database.azure.com", "sidewerzsl", "G4M516SRX7M6BUEM$", "note95-database", 3306, MYSQLI_CLIENT_SSL);
+	mysqli_real_connect($conn, "digitalnotes-server.mysql.database.azure.com", "abvnnoaoog", "005D4TF81D36FI4I$", "digitalnotes-database", 3306, MYSQLI_CLIENT_SSL);
 
 
     $sql = "SELECT * FROM note_table WHERE id=$id";
@@ -566,7 +566,7 @@ document.addEventListener('mousemove', function(event) {
                 // Create connection
                 $conn = mysqli_init(); 
 	            mysqli_ssl_set($conn,NULL,NULL, "DigiCertGlobalRootCA.crt.pem", NULL, NULL); 
-	            mysqli_real_connect($conn, "note95-server.mysql.database.azure.com", "sidewerzsl", "G4M516SRX7M6BUEM$", "note95-database", 3306, MYSQLI_CLIENT_SSL);
+	            mysqli_real_connect($conn, "digitalnotes-server.mysql.database.azure.com", "abvnnoaoog", "005D4TF81D36FI4I$", "digitalnotes-database", 3306, MYSQLI_CLIENT_SSL);
 
 
                 if (isset($_GET['id'])) {
