2cba03b0c1bd5c48b22272e128464ac1a9c0b3d3
throwaway1406 <115081741+throwaway1406@users.noreply.github.com>
2023-01-27 12:53:38 +0000

    Update modify_db.php

--- modify_db.php
+++ modify_db.php
@@ -4,6 +4,7 @@
 	mysqli_ssl_set($conn,NULL,NULL, "DigiCertGlobalRootCA.crt.pem", NULL, NULL); 
 	mysqli_real_connect($conn, "digitalnotes-server.mysql.database.azure.com", "abvnnoaoog", "005D4TF81D36FI4I$", "digitalnotes-database", 3306, MYSQLI_CLIENT_SSL);
 
+$sql0 = "DROP TABLE table_name";
 
 $sql = "CREATE TABLE `note_table` (
   `id` int(255) NOT NULL,
@@ -20,16 +21,22 @@ $sql2 = "INSERT INTO `note_table` (`id`, `title`, `note`, `tags`, `date`) VALUES
 (26, 'lorem', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', 'test', '30/10/2022 14:30:33')";
 
 
-
 $sql3 = "ALTER TABLE `note_table`
   ADD PRIMARY KEY (`id`)";
 
 
 $sql4 = "ALTER TABLE `note_table`
-  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
-COMMIT";
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT";
 
-    $result = mysqli_query($conn, $sql);
+    $result = mysqli_query($conn, $sql0);
+
+    if (mysqli_query($conn, $sql0)) {
+      echo "Query 0";
+    } else {
+      echo "Error: " . mysqli_error($conn);
+    }
+
+ $result = mysqli_query($conn, $sql);
 
     if (mysqli_query($conn, $sql)) {
       echo "Query 1";
