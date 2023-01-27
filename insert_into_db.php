30acb6bb19c09f50140bed643e60dc1611a08231
throwaway1406 <115081741+throwaway1406@users.noreply.github.com>
2023-01-27 12:28:51 +0000

    Update insert_into_db.php

--- insert_into_db.php
+++ insert_into_db.php
@@ -8,7 +8,7 @@ $dbname = "test";
 
     $conn = mysqli_init(); 
 	mysqli_ssl_set($conn,NULL,NULL, "DigiCertGlobalRootCA.crt.pem", NULL, NULL); 
-	mysqli_real_connect($conn, "note95-server.mysql.database.azure.com", "sidewerzsl", "G4M516SRX7M6BUEM$", "note95-database", 3306, MYSQLI_CLIENT_SSL);
+	mysqli_real_connect($conn, "digitalnotes-server.mysql.database.azure.com", "abvnnoaoog", "005D4TF81D36FI4I", "digitalnotes-database", 3306, MYSQLI_CLIENT_SSL);
 
 
 $id = $_REQUEST['id'];
