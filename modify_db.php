569ea73e85ee0f7121de74cee2fc167a4b4158b5
throwaway1406 <115081741+throwaway1406@users.noreply.github.com>
2023-01-27 13:05:55 +0000

    Update modify_db.php

--- modify_db.php
+++ modify_db.php
@@ -4,8 +4,6 @@
 	mysqli_ssl_set($conn,NULL,NULL, "DigiCertGlobalRootCA.crt.pem", NULL, NULL); 
 	mysqli_real_connect($conn, "digitalnotes-server.mysql.database.azure.com", "abvnnoaoog", "005D4TF81D36FI4I$", "digitalnotes-database", 3306, MYSQLI_CLIENT_SSL);
 
-$sql0 = "DROP TABLE note_table";
-
 $sql = "CREATE TABLE `note_table` (
   `id` int(255) NOT NULL,
   `title` mediumtext NOT NULL,
@@ -20,14 +18,6 @@ $sql3 = "ALTER TABLE `note_table`
 
 $sql4 = "ALTER TABLE `note_table`
   MODIFY `id` int(255) NOT NULL AUTO_INCREMENT";
-
-    $result = mysqli_query($conn, $sql0);
-
-    if (mysqli_query($conn, $sql0)) {
-      echo "Query 0";
-    } else {
-      echo "Error: " . mysqli_error($conn);
-    }
 
  $result = mysqli_query($conn, $sql);
 
